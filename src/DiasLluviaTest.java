b2faf12f70b4a7eb8f0dcb2a70bdf43a69775527
Isanvil <isaac.isanvil@gmail.com>
2024-03-11 16:50:08 +0100

    Tercer commit. Tests del primer método.

--- src/DiasLluviaTest.java
+++ src/DiasLluviaTest.java
@@ -1,2 +1,24 @@
-public class DiasLluviaTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class DiasLluviaTest {
+    @Test
+    public void testDia32Mes1True_devuelveFalse(){
+        DiasLluvia d = new DiasLluvia();
+        assertEquals(false, d.registroDia(32, 1, true));
+    }
+
+    @Test
+    public void testDia1Mes13True_devuelveFalse(){
+        DiasLluvia d = new DiasLluvia();
+        assertEquals(false, d.registroDia(1, 13, true));
+    }
+
+    @Test
+    public void testDia12Mes12False_devuelveTrue(){
+        DiasLluvia d = new DiasLluvia();
+        assertNotEquals(true, d.registroDia(1, 1, false));
+    }
 }
